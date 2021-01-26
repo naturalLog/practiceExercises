c2680229a69debf9cea0d49c5200adcd9b0c7fcd
naturalLog <ellen.khau@gmail.com>
2021-01-26 15:27:11 -0500

    odetofood

---
+++ OdeToFood/OdeToFood/OdeToFood.Web/Api/RestaurantsController.cs
@@ -0,0 +1,28 @@
+﻿using OdeToFood.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Web.Api
+{
+    public class RestaurantsController : ApiController
+    {
+        private readonly IRestaurantData db;
+        public RestaurantsController(IRestaurantData db)
+        {
+            this.db = db;
+        }
+
+        public IRestaurantData Db { get; }
+
+        public IEnumerable<Restaurant> Get()
+        {
+            var model = db.GetAll();
+            return model;
+        }
+    }
+}
